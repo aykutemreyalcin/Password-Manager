b170e6d442b496104c1aa08ca20be59e8080749e
aykutemreyalcin <aykutemyeyalcinn@gmail.com>
2025-03-16 15:25:40 +0100

    adding the password generator

---
+++ main.py
@@ -0,0 +1,8 @@
+import json
+import secrets
+import string
+
+def generate_password(length = 12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(secrets.choice(characters) for i in range(length))
+
